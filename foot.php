e39053831237e05a164820c2a97081e7e9404a6a
1707rosa <160504503+1707rosa@users.noreply.github.com>
2025-05-21 21:40:49 -0400

    tarea 1

---
+++ foot.php
@@ -0,0 +1,21 @@
+ <style>
+     #divFooter {
+            background: rgb(179, 128, 162);
+            color: #fff;
+            text-align: center;
+            padding: 10px 0;
+            position: relative;
+            bottom: 0;
+            width: 100%;
+            border-radius: 5px;
+            margin-top: 20px;
+            
+        }
+</style>
+ 
+ <div id="divFooter">
+            <p>Desarrollado por: Rosa Sanchez</p>
+        </div>
+    </div>
+</body>
+</html>
---
+++ head.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color:rgb(238, 144, 199);
+            margin: 0;
+            padding: 0;
+        }
+        #mitarea1 {
+            width: 80%;
+            margin: auto;
+            overflow: hidden;
+        }
+        #divMenu {
+            background: rgb(179, 128, 162);
+            color: #fff;
+            padding: 10px 0;
+            text-align: center;
+            border-radius: 5px;
+        }
+        #divMenu ul {
+            list-style: none;
+            padding: 0;
+        }
+        #divMenu ul li {
+            display: inline;
+            margin-right: 20px;
+        }
+        #divMenu ul li a {
+            color: #fff;
+            text-decoration: none;
+            
+        }
+        #divMenu ul li a:hover {
+            text-decoration: underline;
+        }
+        #divContenido {
+            background: rgb(212, 116, 172);
+            padding: 20px;
+            margin-top: 20px;
+            margin-bottom: 20px;
+            border-radius: 5px;
+            min-height: 200px;
+
+        }
+       
+        h1, p,h2,h3 {
+            color: rgb(255, 255, 255);
+        }
+    </style>
+</head>
+<body>
+    <div id="mitarea1">
+        <div>
+            <h1>Tarea 1</h1>
+            <p>Esta es la tarea 1 de programacion web</p>
+        </div>
+        
+        <div id="divMenu">
+            <ul>
+                <li><a href="tarjeta.php">Tarjeta</a></li>
+                <li><a href="calculadora.php">Calculadora</a></li>
+                <li><a href="adivina.php">Adivina</a></li>
+                <li><a href="acercade.php">Acerca de</a></li>
+            </ul>
+        </div>
+        <div id="divContenido">
+       
+    
+  
